e077f4b62ae2b28ca2bb0a3450df46bcaf88f0b3
DurgaSainath189 <durgasainathdora@gmail.com>
2024-11-09 19:17:18 +0530

    uploading docs is done

--- components/MobileNavigation.tsx
+++ components/MobileNavigation.tsx
@@ -3,8 +3,6 @@
 import {
   Sheet,
   SheetContent,
-  SheetDescription,
-  SheetHeader,
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
@@ -99,7 +97,7 @@ const MobileNavigation = ({
           </nav>
           <Separator className="my-5 bg-light-200/20" />
           <div className="flex flex-col justify-between gap-5 pb-5">
-            <FileUploader />
+            <FileUploader ownerId={ownerId} accountId={accountId} />
             <Button
               type="submit"
               className="mobile-sign-out-button"
