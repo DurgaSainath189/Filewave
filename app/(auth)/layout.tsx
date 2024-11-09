70fca0b17bc7c9833d037fc7b60659592938f090
DurgaSainath189 <durgasainathdora@gmail.com>
2024-11-09 11:09:47 +0530

    auth form is done

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -30,7 +30,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
       <section className="flex flex-1 flex-col items-center bg-white p-4 py-10 lg:justify-center lg:p-10 lg:py-0">
         <div className="mg-16 lg:hidden">
           <Image
-            src="/assets/icons/logo-full-brand.svg"
+            src="/logo2.png"
             alt="filewave_logo"
             width={224}
             height={82}
